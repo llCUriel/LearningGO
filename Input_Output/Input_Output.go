27635047abc8fe1d5afd3991f1a21509d453473c
llCUriel <48939025+llCUriel@users.noreply.github.com>
2020-03-27 14:53:08 -0600

    commit

--- Input_Output/Input_Output.go
+++ Input_Output/Input_Output.go
@@ -1,18 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
+	str := "Cesar"
+	fmt.Printf("Hello %v \n", str)
 	var yourAge int
 	fmt.Println("What's your age?")
 	fmt.Scanf("%d\n", &yourAge)
-	fmt.Printf("%d\n", yourAge)
-	Myage := 22
-	str := "Cesar"
-	flag := true
-	fmt.Printf("%v \n", flag)
-	fmt.Printf("%v \n", Myage)
-	fmt.Printf("%v \n", str)
+	fmt.Printf("Your age is %d\n", yourAge)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("What's your favorite animal?")
+	text, err := reader.ReadString('\n')
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(text)
+	}
+
 }
