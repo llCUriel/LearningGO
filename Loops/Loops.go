a2917f75c6d5acc5992fd3c2f68a3ca468463ce5
llCUriel <48939025+llCUriel@users.noreply.github.com>
2020-03-27 17:02:31 -0600

    commit

--- Loops/Loops.go
+++ Loops/Loops.go
@@ -6,7 +6,7 @@ func main() {
 	//1
 	for i := 1; i <= 10; i++ {
 		for j := 1; j <= 10; j++ {
-			//fmt.Printf("%d x %d = %d \n", i, j, i*j)
+			fmt.Printf("%d x %d = %d \n", i, j, i*j)
 		}
 	}
 	//2
@@ -15,7 +15,7 @@ func main() {
 	for i <= 10 {
 		j = 1
 		for j <= 10 {
-			//fmt.Printf("%d x %d = %d \n", i, j, i*j)
+			fmt.Printf("%d x %d = %d \n", i, j, i*j)
 			j++
 		}
 		i++
@@ -26,7 +26,7 @@ func main() {
 	for {
 		o = 1
 		for {
-			//fmt.Printf("%d x %d = %d \n", k, o, k*o)
+			fmt.Printf("%d x %d = %d \n", k, o, k*o)
 			o++
 			if o > 10 {
 				break
