ea3087546a2a0ab73f2506ff3cf128d522d31914
llCUriel <48939025+llCUriel@users.noreply.github.com>
2020-04-01 22:08:28 -0600

    commit

--- Methods/Methods.go
+++ Methods/Methods.go
@@ -7,8 +7,16 @@ type User struct {
 	nombre, apellido string
 }
 
-func (this User) nombre_completo() string {
-	return this.nombre + " " + this.apellido
+func (first User) nombrecompleto() string {
+	return first.nombre + " " + first.apellido
+}
+
+func (second User) setname1(name string) {
+	second.nombre = name
+}
+
+func (third *User) setname2(name string) {
+	third.nombre = name
 }
 
 func main() {
@@ -25,5 +33,10 @@ func main() {
 		fmt.Println("y")
 	}
 
-	fmt.Println(cesar.nombre_completo())
+	fmt.Println(cesar.nombrecompleto())
+
+	cesar.setname1("Marcos")
+	fmt.Println(cesar.nombre)
+	cesar.setname2("Marcos")
+	fmt.Println(cesar.nombre)
 }
